dcfb2212afc48c3c3e7ebff291d7223e852a3af1
cygbbhx <cygbbhx@yonsei.ac.kr>
2024-05-20 15:17:45 +0000

    Feat: Add custom Eval

--- tools/demo_track.py
+++ tools/demo_track.py
@@ -15,6 +15,7 @@ from yolox.tracker.byte_tracker import BYTETracker
 from yolox.ours.reid_tracker import ReIdTracker
 from yolox.tracking_utils.timer import Timer
 from ultralytics import YOLO
+from custom_eval import evaluate_result
 
 IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
 
@@ -233,6 +234,7 @@ def image_demo(predictor, vis_folder, current_time, args):
         res_file = osp.join(vis_folder, f"{timestamp}.txt")
         with open(res_file, 'w') as f:
             f.writelines(results)
+        evaluate_result(dest=res_file)
         logger.info(f"save results to {res_file}")
 
 
