d63ce92359d0288c6766b0d072c671dc4b6a5f78
JungKH33 <melon345@yonsei.ac.kr>
2024-06-04 14:11:42 +0000

    Feat: Add LSTM motion model

--- yolox/ours/lstm_tracker.py
+++ yolox/ours/lstm_tracker.py
@@ -1,4 +1,243 @@
-from .byte_tracker import BYTETracker
+from yolox.tracker.byte_tracker import BYTETracker, KalmanFilter, TrackState, joint_stracks, sub_stracks, remove_duplicate_stracks
+from .basetrack import BaseTrack, TrackState
+from yolox.tracker import matching
+import torch.nn as nn
+import torch
+import numpy as np
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+class STrack(BaseTrack):
+    shared_kalman = KalmanFilter()
+    def __init__(self, tlwh, score):
+
+        # wait activate
+        self._tlwh = np.asarray(tlwh, dtype=np.float)
+        self.kalman_filter = None
+        self.mean, self.covariance = None, None
+        self.is_activated = False
+
+        self.score = score
+        self.tracklet_len = 0
+
+        self.lstm_tlwh = None
+        self.predict_tlwh = None 
+        model_load_path = '/home/work/YaiBawi/kyunghoon/YaiBawi/yolox/ours/lstm.pth'
+        input_size = 4  # x, y, w, h
+        hidden_size = 50
+        output_size = 4
+        
+        self.width = 912
+        self.height = 500
+        
+        self.lstm = LSTMModel(input_size, hidden_size, output_size)
+        self.lstm.load_state_dict(torch.load(model_load_path))
+        self.lstm.eval()
+        self.store_tlwh(self.normalize_tlwh(self._tlwh)) 
+        
+
+    def predict(self):
+        mean_state = self.mean.copy()
+        if self.state != TrackState.Tracked:
+            mean_state[7] = 0
+        self.mean, self.covariance = self.kalman_filter.predict(mean_state, self.covariance)
+
+    @staticmethod
+    def multi_predict(stracks):
+        if len(stracks) > 0:
+            multi_mean = np.asarray([st.mean.copy() for st in stracks])
+            multi_covariance = np.asarray([st.covariance for st in stracks])
+            for i, st in enumerate(stracks):
+                if st.state != TrackState.Tracked:
+                    multi_mean[i][7] = 0
+            multi_mean, multi_covariance = STrack.shared_kalman.multi_predict(multi_mean, multi_covariance)
+            for i, (mean, cov) in enumerate(zip(multi_mean, multi_covariance)):
+                stracks[i].mean = mean
+                stracks[i].covariance = cov
+
+    def activate(self, kalman_filter, frame_id):
+        """Start a new tracklet"""
+        self.kalman_filter = kalman_filter
+        self.track_id = self.next_id()
+        self.mean, self.covariance = self.kalman_filter.initiate(self.tlwh_to_xyah(self._tlwh))
+
+        self.tracklet_len = 0
+        self.state = TrackState.Tracked
+        if frame_id == 1:
+            self.is_activated = True
+        # self.is_activated = True
+        self.frame_id = frame_id
+        self.start_frame = frame_id
+
+    def re_activate(self, new_track, frame_id, new_id=False):
+        self.mean, self.covariance = self.kalman_filter.update(
+            self.mean, self.covariance, self.tlwh_to_xyah(new_track.tlwh)
+        )
+        self.tracklet_len = 0
+        self.state = TrackState.Tracked
+        self.is_activated = True
+        self.frame_id = frame_id
+        if new_id:
+            self.track_id = self.next_id()
+        self.score = new_track.score
+
+    def update(self, new_track, frame_id):
+        """
+        Update a matched track
+        :type new_track: STrack
+        :type frame_id: int
+        :type update_feature: bool
+        :return:
+        """
+        self.frame_id = frame_id
+        self.tracklet_len += 1
+
+        new_tlwh = new_track.tlwh
+        # import pdb; pdb.set_trace()
+        self.mean, self.covariance = self.kalman_filter.update(
+            self.mean, self.covariance, self.tlwh_to_xyah(new_tlwh))
+
+        norm_tlwh = self.normalize_tlwh(new_tlwh)
+        self.store_tlwh(norm_tlwh)
+        self.predict_tlwh = self.lstm(self.lstm_tlwh)
+        
+        self.new_mean = self.predict_tlwh.squeeze(0).squeeze(0).detach().numpy()
+        self.new_mean = self.unnormalize_tlwh(self.new_mean)
+        self.new_mean = self.tlwh_to_xyah(self.new_mean)
+        self.mean[:4] = self.new_mean
+        
+        self.state = TrackState.Tracked
+        self.is_activated = True
+
+        self.score = new_track.score
+
+    def normalize_tlwh(self, tlwh):
+        """Normalize the bounding box coordinates."""
+        tl_x, tl_y, w, h = tlwh
+        norm_tl_x = tl_x / self.width
+        norm_tl_y = tl_y / self.height
+        norm_w = w / self.width
+        norm_h = h / self.height
+
+        return torch.tensor([norm_tl_x, norm_tl_y, norm_w, norm_h], dtype=torch.float32)
+
+    def unnormalize_tlwh(self, tlwh):
+        norm_tl_x, norm_tl_y, norm_w, norm_h = tlwh
+
+        tl_x = norm_tl_x * self.width
+        tl_y = norm_tl_y * self.height
+        w = norm_w * self.width
+        h = norm_h * self.height
+
+        return np.array([tl_x, tl_y, w, h])
+
+    def store_tlwh(self,tlwh):
+        # Convert tlwh to a PyTorch tensor if it isn't already
+        tlwh_tensor = torch.tensor(tlwh) if not isinstance(tlwh, torch.Tensor) else tlwh
+
+        if self.lstm_tlwh is None:
+            # Initialize self.lstm_tlwh if it doesn't exist, with batch size 1
+            self.lstm_tlwh = tlwh_tensor.unsqueeze(0).unsqueeze(0)  # Add two new dimensions
+        else:
+            # Stack the new tensor on the existing one, maintaining batch size 1
+            self.lstm_tlwh = torch.cat((self.lstm_tlwh, tlwh_tensor.unsqueeze(0).unsqueeze(0)), dim=1)
+
+            # If the number of tensors exceeds 10, discard the oldest one
+            if self.lstm_tlwh.size(1) > 10:
+                self.lstm_tlwh = self.lstm_tlwh[:, 1:, :]
+
+    def load_tlwh(self):
+        if self.lstm_tlwh is None:
+            return None
+        return self.lstm_tlwh[-1]
+
+    @property
+    def tlwh(self):
+        """Get current position in bounding box format `(top left x, top left y,
+                width, height)`.
+        """
+        if self.mean is None:
+            return self._tlwh.copy()
+        ret = self.mean[:4].copy()
+        ret[2] *= ret[3]
+        ret[:2] -= ret[2:] / 2
+        return ret
+    # @jit(nopython=True)
+
+    # def tlwh(self):
+    #     """Get current position in bounding box format `(top left x, top left y,
+    #             width, height)`.
+    #     """
+
+    #     if self.predict_tlwh is None:
+    #         return self._tlwh
+
+    #     else:
+    #         norm_tl_x, norm_tl_y, norm_w, norm_h = self.predict_tlwh.squeeze(0).squeeze(0).detach().numpy()
+
+    #         tl_x = norm_tl_x * self.width
+    #         tl_y = norm_tl_y * self.height
+    #         w = norm_w * self.width
+    #         h = norm_h * self.height
+
+    #         return np.array([tl_x, tl_y, w, h])
+
+    @property
+    # @jit(nopython=True)
+    def tlbr(self):
+        """Convert bounding box to format `(min x, min y, max x, max y)`, i.e.,
+        `(top left, bottom right)`.
+        """
+        ret = self.tlwh.copy()
+        ret[2:] += ret[:2]
+        return ret
+
+    @staticmethod
+    # @jit(nopython=True)
+    def tlwh_to_xyah(tlwh):
+        """Convert bounding box to format `(center x, center y, aspect ratio,
+        height)`, where the aspect ratio is `width / height`.
+        """
+        ret = np.asarray(tlwh).copy()
+        ret[:2] += ret[2:] / 2
+        ret[2] /= ret[3]
+        return ret
+
+    def to_xyah(self):
+        return self.tlwh_to_xyah(self.tlwh)
+
+    @staticmethod
+    # @jit(nopython=True)
+    def tlbr_to_tlwh(tlbr):
+        ret = np.asarray(tlbr).copy()
+        ret[2:] -= ret[:2]
+        return ret
+
+    @staticmethod
+    # @jit(nopython=True)
+    def tlwh_to_tlbr(tlwh):
+        ret = np.asarray(tlwh).copy()
+        ret[2:] += ret[:2]
+        return ret
+
+    def __repr__(self):
+        return 'OT_{}_({}-{})'.format(self.track_id, self.start_frame, self.end_frame)
+
 
 class CustomMotionTracker(BYTETracker):
     def __init__(self, args, frame_rate=30):
@@ -13,6 +252,14 @@ class CustomMotionTracker(BYTETracker):
         self.buffer_size = int(frame_rate / 30.0 * args.track_buffer)
         self.max_time_lost = self.buffer_size
         self.kalman_filter = KalmanFilter()
+
+        input_size = 4  # x, y, w, h
+        hidden_size = 50
+        output_size = 4
+        model_load_path = '/home/work/YaiBawi/kyunghoon/YaiBawi/yolox/ours/lstm.pth'
+        self.lstm = LSTMModel(input_size, hidden_size, output_size)
+        self.lstm.load_state_dict(torch.load(model_load_path))
+        self.lstm.eval()
 
     def update(self, output_results, img_info, img_size):
         self.frame_id += 1
@@ -63,6 +310,7 @@ class CustomMotionTracker(BYTETracker):
         ''' Step 2: First association, with high score detection boxes'''
         strack_pool = joint_stracks(tracked_stracks, self.lost_stracks)
         # Predict the current location with KF
+        # import pdb; pdb.set_trace()
         STrack.multi_predict(strack_pool)
         dists = matching.iou_distance(strack_pool, detections)
         if not self.args.mot20:
